46142bbcd55b9442cf70169ac110bea0441ecc09
deaaww <deamarsell12@gmail.com>
2024-10-22 18:00:39 +0700

    simpan kode java ke Github

---
+++ MyFirstJava08.java
@@ -0,0 +1,18 @@
+/**
+ * Tugas08
+ */
+public class Tugas08 {
+    public static void main (String[] args){
+        System.out.println ("--------------------------------------------------------");
+        System.out.println ("--Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println ("--------------------------------------------------------");
+        System.out.println ("Nama       : Dea Marselia Rahma");
+        System.out.println ("NIM        : 244107060087");
+        System.out.println ("No Absen   : 08");
+        System.out.println ("Prodi      : D-IV Sistem Informasi Bisnis");
+        System.out.println ("Alamat     : Jl. Loandeng No. 111, Kalisongo, Dau, Kabupaten Malang");
+        System.out.println ("No HP      : 0895397301466");
+        System.out.println ("Hobi       : Membaca");
+        
+    }
+}
